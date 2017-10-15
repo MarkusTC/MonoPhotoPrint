5225d95d1c951f6fdde83bab8d8df29903d595ca
unknown-user <unknown-user@192.168.178.45>
2017-10-15 20:28:41 +0200

    Erstes Einchecken des Moduls PhotoPrinter

---
+++ PhotoPrinter/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace PhotoPrinter
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+
+			Console.ReadKey ();
+		}
+	}
+}
